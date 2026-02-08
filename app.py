d626183dead219302a55bc97aaa32701ce8c1f85
Giang Lam <lamtunggiang.it@gmail.com>
2026-02-08 11:33:35 +0700

    Edit Code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("---------------------------------")
 print("Xin chao! Day la ung dung Python.")
 print("Duoc chay boi Jenkins CI.")
-print("Duoc sửa bởi giangltapple.")
+print("Duoc add bởi giangltapple.")
 
 print("---------------------------------")
