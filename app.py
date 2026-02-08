0638d376060edad91d925607a61cc610704ca7b1
Giang Lam <lamtunggiang.it@gmail.com>
2026-02-08 11:20:31 +0700

    Fix lỗi

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ print("---------------------------------")
 print("Xin chao! Day la ung dung Python.")
 print("Duoc chay boi Jenkins CI.")
 print("Duoc sửa bởi giangltapple.")
-print("Thieu dau ngoac dong"
+print("Thieu dau ngoac dong")
 print("---------------------------------")
