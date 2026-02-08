607a90585e9d4917228c7899e1e399a793f1493f
giangltapple <gianglt.apple@gmail.com>
2026-02-08 10:00:43 +0700

    Add greeting message from giangltapple

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 print("---------------------------------")
 print("Xin chao! Day la ung dung Python.")
 print("Duoc chay boi Jenkins CI.")
+print("Duoc sửa bởi giangltapple.")
 print("---------------------------------")
